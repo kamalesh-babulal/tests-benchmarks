292452ec20fd88a42143fd33ef3474545ab47bb8
Kamalesh Babulal <kamalesh.babulal@oracle.com>
2023-07-27 08:52:24 +0530

    cgroup: add simple cgroup creator (breadth) test
    This test case times the creation of 1000 directories at root level
    the cpu controller hierarchy.
    
    		/sys/fs/cgroup/cpu/
      		/sys/fs/cgroup/cpu/1
      		/sys/fs/cgroup/cpu/2
      		.....
      		/sys/fs/cgroup/cpu/1000
    
    Signed-off-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>

---
+++ linux-kernel/cgroups/simple-cgroup-creator-breadth.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+source utils.sh
+
+ITERATIONS=1000
+
+last_dir=""
+function die() {
+	echo "$*" 1>&2
+	exit 1
+}
+
+function create_cgroups() {
+	cpu_controller="$1"
+	cd "$cpu_controller" || die "Failed to change dir $cpu_controller"
+	for cgrp in $(seq 1 "$ITERATIONS")
+	do
+		mkdir "$cgrp" || die "Failed to create dir $cgrp"
+	done
+}
+
+function delete_cgroups() {
+	cpu_controller="$1"
+	cd "$cpu_controller" || die "Failed to change dir $cpu_controller"
+	for cgrp in $(seq 1 "$ITERATIONS")
+	do
+		rmdir "$cgrp" || die "Failed to delete dir $cgrp"
+	done
+}
+
+cpu_controller=$(find_controller_mount_point "cpu")
+echo -n "Time to create $ITERATIONS directories breadth"
+time create_cgroups "$cpu_controller"
+
+sleep 1
+
+echo -n "Time to delete $ITERATIONS directories in breadth"
+time delete_cgroups "$cpu_controller"
+
+exit 0
