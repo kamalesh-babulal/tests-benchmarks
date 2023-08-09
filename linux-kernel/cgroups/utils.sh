kamalesh-babulal/tests-benchmarks

commit 25886c1f921c7feb0288d20357024d49a4f26dac

utils.sh: fix find_controller_mount_point() for cgroup v2
Fix the bug with cgroup v2 cgroup setup mode, where the we grep for the
controller name in the /proc/mounts, which will always return false,
because with cgroup v2 it unified hierarchy.

Signed-off-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>

linux-kernel/cgroups/utils.sh
@@ -38,6 +38,7 @@ function print_env() {
 }
 
 function find_controller_mount_point() {
+	cgroupv2=0
 	if [ "$1" == "cpu" ]; then
 		mount_point=$(grep "cgroup" /proc/mounts|grep "$1"|grep -v cpuset|awk '{print $2}')
 	else
@@ -46,6 +47,7 @@ function find_controller_mount_point() {
 
 	if [ -z "$mount_point" ]; then
 		mount_point=$(grep "cgroup2" /proc/mounts|grep "$1"|awk '{print $2}')
+		cgroupv2=1
 	fi
 
 	if [ -z "$mount_point" ]; then
@@ -54,7 +56,7 @@ function find_controller_mount_point() {
 	fi
 
 	base_name=$(basename "$mount_point")
-	if [[ "$base_name" == "$1" ]] || [[ "$base_name" =~ ^"$1," ]] || [[ "$base_name" =~ ",$1"$ ]]; then
+	if [[ "$base_name" == "$1" ]] || [[ "$base_name" =~ ^"$1," ]] || [[ "$base_name" =~ ",$1"$ ]] || [[ "$cgroupv2" -eq 1 ]]; then
 		echo "$mount_point"
 		return
 	fi

commit 597eda684dd95d954ed28fb6e415bafa0229425a

utils.sh: fix the command to find cgroupv2 mount point
The commit 25886c1f921c ("utils.sh: fix find_controller_mount_point()
for cgroup v2") attempted to fix the logic to find the mount point of a
given controller in cgroupv2 setup but failed to remove the grep for the
controller (which is valid only for cgroup v1). This patch fixes it by
removing the extra grep.

Fixes: 25886c1f921c ("utils.sh: fix find_controller_mount_point() for cgroupv2)
Signed-off-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>

linux-kernel/cgroups/utils.sh
@@ -46,7 +46,7 @@ function find_controller_mount_point() {
 	fi
 
 	if [ -z "$mount_point" ]; then
-		mount_point=$(grep "cgroup2" /proc/mounts|grep "$1"|awk '{print $2}')
+		mount_point=$(grep "cgroup2" /proc/mounts|awk '{print $2}')
 		cgroupv2=1
 	fi
 